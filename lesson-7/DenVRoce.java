2a17a0f5cbe4e2974664ce00d06481990c174067
Scytheroid <bohdan.kopcak@gmail.com>
2018-11-11 18:13:03 +0100

    Comments added

--- lesson-7/DenVRoce.java
+++ lesson-7/DenVRoce.java
@@ -1,4 +1,40 @@
+import javax.lang.model.util.ElementScanner6;
+
+/* DenVRoce spočítá rekurzivně počet dní v předešlých uběhlých měsících
+   DenVRoce rozlišuje přestupný rok
+*/
 public class DenVRoce {
+    public static int DenVMesici(int mesic, int rok) {
+        int dny = 0;
+        if (mesic == 0) { return dny; }
+        if ((mesic == 2) && (rok % 4 == 0)) {
+            dny += 28; 
+        }
+
+        else if (mesic == 2) {
+            dny += 29; 
+        }
+
+        else if ((mesic == 1)  || (mesic == 3)  ||
+                (mesic == 5)  ||
+                (mesic == 7)  ||
+                (mesic == 8)  ||
+                (mesic == 10) ||
+                (mesic == 12))
+        {
+            dny += 31;
+        }
+
+        else {dny += 30;}
+
+        return (dny + DenVMesici(mesic - 1, rok));
+
+    }
+
+/*
+    Tělo programu načte datum z jednoho řádku a poté jednotlivá čísla
+    uloží do pole knihovny awh
+*/
     public static void main(String[] args) {
         awh.IntList datum = awh.IntList.create();
 
@@ -6,7 +42,14 @@ public class DenVRoce {
         while(sc.hasNextInt()) {
             datum.add(sc.nextInt());
         }
-        datum.reverse();
-        System.out.printf("%d", datum.get(0));
+        System.out.printf("%d", 
+            (
+                // Počet celých uběhlých měsíců je o 1 menší
+                DenVMesici((datum.get(1) - 1), datum.get(0))
+                + datum.get(2)
+                // Součet počtu dní v uběhlých měsících a počtu uběhlých dní
+                //      aktuálního měsíce.
+            )
+        );
     }
 }
