5c9b00dc88512f966accde75b90a0814ed11e089
Scytheroid <43240517+Scytheroid@users.noreply.github.com>
2018-11-15 14:35:13 +0100

    Factorial function added, code still not functional

--- lesson-7/kombinacniCislo.java
+++ lesson-7/kombinacniCislo.java
@@ -1,9 +1,20 @@
-﻿public class kombinacniCislo {
+public class kombinacniCislo {
     public static void main(String[] args) {
         System.out.printf("Vypocet kombinaciho cisla\n\n");
         System.out.printf("Zadejte velikost mnoziny n: ");
         int n = Integer.parseInt(args[0]);¨
         System.out.printf("\nZadejte pozadovanou velikost vyberu k: ");
         int k = Integer.parseInt(args[1]);
+        
+        int result = 0;
+        result = (faktorial(n) / (faktorial(k) // Yet to be done
     }
+    
+    public static int faktorial(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        else {
+            return (num * faktorial(num - 1))
+        } 
 }
