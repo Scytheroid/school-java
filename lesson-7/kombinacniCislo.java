0b2b46cb9d355bec10d179b70cd20c1a15c63eb7
Scytheroid <43240517+Scytheroid@users.noreply.github.com>
2018-11-15 13:59:40 +0100

    Unfinished homework source code added.

---
+++ lesson-7/kombinacniCislo.java
@@ -0,0 +1,9 @@
+﻿public class kombinacniCislo {
+    public static void main(String[] args) {
+        System.out.printf("Vypocet kombinaciho cisla\n\n");
+        System.out.printf("Zadejte velikost mnoziny n: ");
+        int n = Integer.parseInt(args[0]);¨
+        System.out.printf("\nZadejte pozadovanou velikost vyberu k: ");
+        int k = Integer.parseInt(args[1]);
+    }
+}
